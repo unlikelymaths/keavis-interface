57eac82118b71deb8b2d01ab2b8727d8db09ca05
Markus Plack <markus.plack@gmail.com>
2019-05-03 15:27:12 +0200

    Add time labels to x axis on bar chart
    To fit the scale margins are defined around the chart for the sides as
    well as the top and the bottom. The scales use d3 axis with fixed
    labels. The ticks depend on the size of the svg.

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import { scaleLinear } from 'd3-scale'
 import { max } from 'd3-array'
 import { select } from 'd3-selection'
-import {transition} from "d3-transition"
+import { transition } from "d3-transition"
+import { axisBottom } from "d3-axis"
 import ReactResizeDetector from 'react-resize-detector';
 
 import topicBuffer from '../TopicBuffer'
@@ -66,7 +67,7 @@ class BarChart extends Component {
         if (this.svgRef.current === null) {
             return
         }
-        const node = this.svgRef.current
+        var node = select(this.svgRef.current)
         
         // Foreground data is optional
         var fg_data = []
@@ -75,57 +76,54 @@ class BarChart extends Component {
         }
         
         // Initialize scales
-        const binWidth = this.width / bg_data.length
+        const side_margins = 40
+        const lower_margin = 40
+        const upper_margin = 40
+        const binWidth = (this.width - 2 * side_margins) / bg_data.length
         var dataMax = max(bg_data)
         if (fg_data.length > 0) {
             dataMax = max(fg_data) * 3
         }
         const yScale = scaleLinear()
             .domain([0, dataMax])
-            .range([0, height * 0.9])
+            .range([0, height - lower_margin - upper_margin])
         const xScale = scaleLinear()
             .domain([0, bg_data.length])
-            .range([0, this.width])
+            .range([side_margins, this.width - side_margins])
         
         // Draw background bars
-        select(node)
-            .selectAll('rect.bg')
+        node.selectAll('rect.bg')
             .data(bg_data)
             .enter()
             .append('rect')
             .attr('class', 'bg')
             .attr('height', d => 0)
-            .attr('y', (d,i) => height)
+            .attr('y', (d,i) => height - lower_margin)
         
-        select(node)
-            .selectAll('rect.bg')
+        node.selectAll('rect.bg')
             .data(bg_data)
             .exit()
             .remove()
             
-        select(node)
-            .selectAll('rect.bg')
+        node.selectAll('rect.bg')
             .data(bg_data)
             .style('fill', '#fe9922')
             .attr('x', (d,i) => xScale(i))
             .attr('width', binWidth)
             
-        select(node)
-            .selectAll('rect.bg')
+        node.selectAll('rect.bg')
             .interrupt()
             
-        select(node)
-            .selectAll('rect.bg')
+        node.selectAll('rect.bg')
             .transition()
             .delay(0)
             .duration(500)
             .attr('height', d => yScale(d))
-            .attr('y', (d,i) => height - yScale(d))
+            .attr('y', (d,i) => height - lower_margin - yScale(d))
             
         // Draw foreground bars
         if (fg_data.length > 0) {  
-            select(node)
-                .selectAll('rect.fg')
+            node.selectAll('rect.fg')
                 .data(fg_data)
                 .enter()
                 .append('rect')
@@ -133,37 +131,53 @@ class BarChart extends Component {
                 .attr('height', d => 0)
                 .attr('y', (d,i) => height - 0)
             
-            select(node)
-                .selectAll('rect.fg')
+            node.selectAll('rect.fg')
                 .data(fg_data)
                 .exit()
                 .remove()
                 
-            select(node)
-                .selectAll('rect.fg')
+            node.selectAll('rect.fg')
                 .data(fg_data)
                 .style('fill', '#0000ff')
                 .attr('x', (d,i) => xScale(i))
                 .attr('width', binWidth)
                 
-            select(node)
-                .selectAll('rect.fg')
+            node.selectAll('rect.fg')
                 .interrupt()
                 
-            select(node)
-                .selectAll('rect.fg')
+            node.selectAll('rect.fg')
                 .transition()
                 .delay(0)
                 .duration(500)
                 .attr('height', d => yScale(d))
                 .attr('y', (d,i) => height - yScale(d))
         } else {
-             select(node)
-                .selectAll('rect.fg')
+             node.selectAll('rect.fg')
                 .data(fg_data)
                 .exit()
                 .remove()
         }
+        
+        node.selectAll('#bar_chart_x_axis')
+            .remove();
+        var tickValues = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+        if (width < 300) {
+            tickValues = [0,12,24]
+        } else if (width < 500) {
+            tickValues = [0,6,12,18,24]
+        } else if (width < 750) {
+            tickValues = [0,3,6,9,12,15,18,21,24]
+        } else if (width < 1300) {
+            tickValues = [0,2,4,6,8,10,12,14,16,18,20,22,24]
+        }
+        var x_axis = axisBottom()
+            .scale(xScale)
+            .tickFormat(d => '' + (d%12==0? 12 : d%12) + ':00 ' + ((d%24<12) ? 'am' : 'pm'))
+            .tickValues(tickValues);
+        node.append('g')
+            .attr('id', 'bar_chart_x_axis')   
+            .attr('transform', 'translate(0,' + (height - 40) + ')')    
+            .call(x_axis);
     }
 
     render() {
